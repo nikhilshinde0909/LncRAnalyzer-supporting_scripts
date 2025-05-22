7d18e7468e04e1e4fb359e155e3abe15206339b9
Nikhil Dhanaji Shinde <87862905+nikhilshinde0909@users.noreply.github.com>
2025-05-22 12:28:51 +0530

    Add files via upload

---
+++ Main_Roc-1.R
@@ -0,0 +1,39 @@
+setwd('/home/mpilab/Performance2/Final_ROC/')
+library(dplyr)
+data <- read.table('ROC_values4.TSV', header = T, sep = '\t')
+sigmoid <- function(x, k = 1) {
+  return(1 / (1 + exp(-k * (x - 0.5))))
+}
+
+data <- data %>% group_by(Approach) %>% mutate(FPR=sigmoid(x = FPR,k=10),
+                                               TPR=sigmoid(TPR,k = 10))
+approach <- c("CPAT-Sorghum", "CPAT-Plants", "CPAT-Human",
+              "FEELnc-Sorghum","FEELnc-Arabidopsis", "FEELnc-Human", 
+              "LncFinder-Sorghum","LncFinder-Plants", "LncFinder-Human",
+              "RNAsamba-Sorghum","RNAsamba-full length","RNAsamba-partial length",
+              "CPC2", "LGC", "PfamScan",
+              "CPPred-Sorghum","CPPred-Plants", "CPPred-Human**")
+# Define the color palette
+cbPalette <- c( "#16537e", "#3d85c6","#9fc5e8", 
+                "#ce7e00", "#bf9000",'#ffe599',
+                "#38761d","#93c47d","#8fce00",
+                "#351c75", "#6a329f", "#b4a7d6",
+                "#744700","#741b47","#f44336",
+                "#d5a6bd","#a64d79","#c90076")
+data$Approach <- factor(x = data$Approach,levels = approach)
+# Make the plot
+library(ggplot2)
+p <- data %>% ggplot(aes(x = FPR, y = TPR, group = Approach, colour = Approach)) +
+  geom_line() +
+  geom_abline(intercept = 0, slope = 1, linetype = 'dashed') +
+  xlim(0, 1) + ylim(0, 1) + theme_bw() +
+  xlab('False Positive Rate') + ylab('True Positive Rate') +
+  theme(text = element_text(size = 10)) +
+  scale_colour_manual(values = cbPalette) +
+  theme( legend.title = element_text(size = 10), legend.text = element_text(size = 8)) +
+  labs(colour = 'LncRNAs identification \n methods')
+
+pdf('Lnc_ROC_common1.pdf', width = 7, height = 5)
+p
+dev.off()
+
